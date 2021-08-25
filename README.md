0b010f73b79eb555adf11d90a658af1f529b286e
Foti Ceci <foti.ceci@my.liu.edu>
2021-08-25 13:48:17 -0400

    Initial upload

---
+++ README.md
@@ -0,0 +1,38 @@
+# Udemy - The Complete 2021 Web Development Bootcamp - Drum Kit Challenge
+
+Drum Kit challenge solution from The Complete 2021 Web Development Bootcamp course on Udemy.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+- Create a drum kit where you can play sounds by either clicking the buttons or pressing appropriate keys. 
+
+### Links
+
+- Solution URL: [https://github.com/fotinh0/drum-kit](https://github.com/fotinh0/drum-kit)
+- Live Site URL: []()
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS 
+- JavaScript
+
+### Useful resources
+- [DevDocs.io](https://devdocs.io/css/)
+
+## Author
+
+- GitHub - [@Fotinh0](https://github.com/fotinh0)
+- Frontend Mentor - [@fotinh0](https://www.frontendmentor.io/profile/fotinh0)
+- LinkedIn - [@Foti-Ceci](https://www.linkedin.com/in/foti-ceci/)
---
+++ index.js
@@ -0,0 +1,55 @@
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){  
+        let buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    })
+} 
+
+document.addEventListener("keydown", function(event){
+    playSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function playSound(key){
+    switch (key){
+        case "w":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+        break;
+        case "a":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "s":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "d":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "j":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        case "k":
+            let kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+        break;
+        case "l":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+        break;
+
+        default: console.log();
+    } 
+}
+
+function buttonAnimation(currentKey){
+    let activeButton = document.querySelector("."+ currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
